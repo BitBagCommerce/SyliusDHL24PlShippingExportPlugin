e57c4a1eea25369bfec59c32bff61f8bcae3b981
Jan Hickiewicz <jan.hickiewicz@bitbag.pl>
2022-07-26 12:49:03 +0200

    Changes in composer.json and ecs.php to enable bitbag coding standard

--- ecs.php
+++ ecs.php
@@ -1,14 +1,14 @@
 <?php
 
-use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
-use SlevomatCodingStandard\Sniffs\Commenting\InlineDocCommentDeclarationSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+    $containerConfigurator->import('vendor/bitbag/coding-standard/ecs.php');
 
-    $containerConfigurator->parameters()->set(Option::SKIP, [
-        VisibilityRequiredFixer::class => ['*Spec.php'],
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
 };
